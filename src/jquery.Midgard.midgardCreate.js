7fcf9db50f63ec05c52cf66dc7db4fc14b388dec
Henri Bergius <henri.bergius@iki.fi>
2011-11-01 15:44:24 +0200

    Port the widgetization branch of Midgard Create here

---
+++ src/jquery.Midgard.midgardCreate.js
@@ -0,0 +1,119 @@
+(function(jQuery, undefined) {
+    jQuery.widget('Midgard.midgardCreate', {
+        options: {
+            statechange: function() {},
+            toolbar: 'full',
+            saveButton: null,
+            state: 'browse',
+            highlightColor: '#67cc08'
+        },
+    
+        _create: function() {
+            this.vie = new VIE({classic: true});
+            this._checkSession();
+            this._enableToolbar();
+            this._saveButton();
+            this._editButton();
+            this.element.midgardStorage({vie: this.vie});
+        },
+        
+        _init: function() {
+            if (this.options.state === 'edit') {
+                this._enableEdit();
+            } else {
+                this._disableEdit();
+            }
+        },
+        
+        _checkSession: function() {
+            if (!Modernizr.sessionstorage) {
+                return;
+            }
+            
+            if (sessionStorage.getItem('Midgard.create.toolbar')) {
+                this._setOption('toolbar', sessionStorage.getItem('Midgard.create.toolbar'));
+            }
+            
+            if (sessionStorage.getItem('Midgard.create.state')) {
+                this._setOption('state', sessionStorage.getItem('Midgard.create.state'));
+            }
+            
+            this.element.bind('midgardcreatestatechange', function(event, state) {
+                sessionStorage.setItem('Midgard.create.state', state);
+            });
+        },
+        
+        _saveButton: function() {
+            if (this.options.saveButton) {
+                return this.options.saveButton;
+            }
+            
+            jQuery('#midgard-bar .toolbarcontent-right').append(jQuery('<button id="midgardcreate-save">Save</button>'));
+            this.options.saveButton = jQuery('#midgardcreate-save');
+            this.options.saveButton.button({disabled: true})
+            return this.options.saveButton;
+        },
+        
+        _editButton: function() {
+            var widget = this;
+            jQuery('#midgard-bar .toolbarcontent-right').append(jQuery('<input type="checkbox" id="midgardcreate-edit" /><label for="midgardcreate-edit">Edit</label>'));
+            var editButton = jQuery('#midgardcreate-edit').button();
+            if (this.options.state === 'edit') {
+                editButton.attr('checked', true);
+                editButton.button('refresh');
+            }
+            editButton.bind('change', function() {
+                if (editButton.attr('checked')) {
+                    widget._enableEdit();
+                    return;
+                }
+                widget._disableEdit();
+            });
+        },
+        
+        _enableToolbar: function() {
+            var widget = this;
+            this.element.bind('toolbarstatechange', function(event, options) {
+                if (Modernizr.sessionstorage) {
+                    sessionStorage.setItem('Midgard.create.toolbar', options.display);
+                }
+                widget._setOption('toolbar', options.display);
+            });
+
+            this.element.midgardToolbar({display: this.options.toolbar, vie: this.vie});
+        },
+        
+        _enableEdit: function() {
+            var widget = this;
+            jQuery('[about]').each(function() {
+                var element = this;
+                var highlightEditable = function(event, options) {
+                    if (options.entityElement.get(0) !== element) {
+                        // Propagated event from another entity, ignore
+                        return;
+                    }
+
+                    // Highlight the editable
+                    options.element.effect('highlight', {color: widget.options.highlightColor}, 3000);
+                };
+                
+                jQuery(this).bind('editableenableproperty', highlightEditable);
+                jQuery(this).bind('editabledisable', function() {
+                    jQuery(this).unbind('editableenableproperty', highlightEditable);
+                });
+                jQuery(this).midgardEditable({disabled: false, vie: widget.vie});
+            });
+            this._setOption('state', 'edit');
+            this._trigger('statechange', null, 'edit');
+        },
+        
+        _disableEdit: function() {
+            var widget = this;
+            jQuery('[about]').each(function() {
+                jQuery(this).midgardEditable({disabled: true, vie: widget.vie}).removeClass('ui-state-disabled');
+            });
+            this._setOption('state', 'browse');
+            this._trigger('statechange', null, 'browse');
+        }
+    })
+})(jQuery);
---
+++ src/jquery.Midgard.midgardEditable.js
@@ -0,0 +1,187 @@
+(function(jQuery, undefined) {
+    jQuery.widget('Midgard.midgardEditable', {
+        options: {
+            editables: [],
+            model: null,
+            editor: 'hallo',
+            addButton: null,
+            enable: function() {},
+            enableproperty: function() {},
+            disable: function() {},
+            activated: function() {},
+            deactivated: function() {},
+            changed: function() {},
+            vie: null
+        },
+    
+        _create: function() {
+            this.vie = this.options.vie;
+            if (!this.options.model) {
+                this.options.model = this.vie.RDFaEntities.getInstance(this.element);
+            }
+        },
+        
+        _init: function() {
+            if (this.options.disabled) {
+                this.disable();
+                return;
+            }
+            this.enable();
+        },
+        
+        enable: function() {      
+            var widget = this;
+            this.vie.RDFa.findPredicateElements(this.options.model.id, jQuery('[property]', this.element), false).each(function() {
+                return widget._enableProperty(jQuery(this));
+            });
+           
+            console.log(this.vie.services.rdfa.views);
+            _.forEach(this.vie.RDFaEntities.CollectionViews, function(view) {
+                widget._enableCollection(view);
+            });
+            
+            this._trigger('enable', null, {
+                instance: this.options.model,
+                entityElement: this.element
+            });
+        },
+        
+        disable: function() {
+            jQuery.each(this.options.editables, function(index, editable) {
+                editable.setUnmodified();
+                if (typeof editable.changeTimer !== 'undefined') {
+                    window.clearInterval(editable.changeTimer);
+                }
+                try {
+                    editable.destroy();
+                } catch (err) {
+                }
+            });
+            this.options.editables = [];
+            
+            if (this.options.addButton) {
+                this.options.addButton.remove();
+                delete this.options.addButton;
+            }
+
+            this._trigger('disable', null, {
+                instance: this.options.model,
+                entityElement: this.element
+            });
+        },
+        
+        _enableProperty: function(element) {
+            var propertyName = this.vie.RDFa.getPredicate(element);
+            if (!propertyName) {
+                return true;
+            }
+            if (this.options.model.get(propertyName) instanceof Array) {
+                // For now we don't deal with multivalued properties in Aloha
+                return true;
+            }
+
+            if (this.options.editor === "aloha") {
+                return this._enableAloha(element);
+            }
+
+            // Default to Hallo
+            jQuery(element).hallo({
+                plugins: {
+                    halloformat: {}
+                },
+                editable: true
+            });
+            jQuery(element).bind('halloactivated', function(event, data) {
+            });
+        },
+
+        _enableAloha: function(element) {
+            var editable = new GENTICS.Aloha.Editable(element);
+            editable.vieEntity = this.options.model;
+
+            // Subscribe to activation and deactivation events
+            var widget = this;
+            GENTICS.Aloha.EventRegistry.subscribe(editable, 'editableActivated', function() {
+                widget._trigger('activated', null, {
+                    editable: editable,
+                    property: propertyName,
+                    instance: widget.options.model,
+                    element: element,
+                    entityElement: this.element
+                });
+            });
+            GENTICS.Aloha.EventRegistry.subscribe(editable, 'editableDeactivated', function() {
+                widget._trigger('deactivated', null, {
+                    editable: editable,
+                    property: propertyName,
+                    instance: widget.options.model,
+                    element: element,
+                    entityElement: this.element
+                });
+            });
+
+            // Register a timer to copy any modified contents
+            // TODO: Replace with smartContentChange when Aloha .10 is out
+            editable.changeTimer = window.setInterval(function() {
+                widget._checkModified(propertyName, editable);
+            }, 2000);
+
+            this._trigger('enableproperty', null, {
+                editable: editable,
+                property: propertyName,
+                instance: this.options.model,
+                element: element,
+                entityElement: this.element
+            });
+            
+            this.options.editables.push(editable);
+        },
+        
+        _enableCollection: function(collectionView) {
+            var widget = this;
+
+            if (this.vie.RDFa.getSubject(collectionView.el) !== this.vie.RDFa._toReference(widget.options.model.id)) {
+                return;
+            }
+
+            if (widget.options.addButton) {
+                return;
+            }
+
+            collectionView.bind('add', function(itemView) {
+                //itemView.el.effect('slide');
+                itemView.model.primaryCollection = collectionView.collection;
+                itemView.el.editable({disabled: widget.options.disabled, model: itemView.model});
+            });
+            
+            collectionView.bind('remove', function(itemView) {
+                itemView.el.hide('drop');
+            });
+            
+            widget.options.addButton = jQuery('<button>Add</button>').button();
+            widget.options.addButton.click(function() {
+                collectionView.collection.add({});
+            });
+            
+            collectionView.el.after(widget.options.addButton);
+        },
+        
+        _checkModified: function(propertyName, editable) {
+            if (!editable.isModified()) {
+                return true;
+            }
+            var changedProperties = {};
+            changedProperties[propertyName] = editable.getContents();
+            editable.setUnmodified();
+            this.options.model.set(changedProperties, {silent: true});
+            
+            this._trigger('changed', null, {
+                editable: editable,
+                property: propertyName,
+                instance: this.options.model,
+                element: editable.obj,
+                entityElement: this.element
+            });
+        }
+    });
+})(jQuery);
