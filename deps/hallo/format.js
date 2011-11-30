fcf440447563333997c700ca38b3f9597ba0d6d7
Henri Bergius <henri.bergius@iki.fi>
2011-11-30 15:48:02 +0200

    New upstream Hallo

--- deps/hallo/format.js
+++ deps/hallo/format.js
@@ -1,24 +1,30 @@
-(function() {
-  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+
   (function(jQuery) {
     return jQuery.widget("IKS.halloformat", {
       options: {
         editable: null,
         toolbar: null,
         uuid: "",
-        formattings: ["bold", "italic", "strikeThrough", "underline"]
+        formattings: {
+          bold: true,
+          italic: true,
+          strikeThrough: true,
+          underline: true
+        }
       },
       _create: function() {
-        var buttonize, buttonset, format, widget, _i, _len, _ref;
+        var buttonize, buttonset, enabled, format, widget, _ref;
+        var _this = this;
         widget = this;
         buttonset = jQuery("<span class=\"" + widget.widgetName + "\"></span>");
-        buttonize = __bind(function(format) {
+        buttonize = function(format) {
           var button, element, id, label, queryState;
           label = format.substr(0, 1).toUpperCase();
-          id = "" + this.options.uuid + "-" + format;
-          buttonset.append(jQuery("<input id=\"" + id + "\" type=\"checkbox\" /><label for=\"" + id + "\">" + label + "</label>").button());
+          id = "" + _this.options.uuid + "-" + format;
+          buttonset.append(jQuery("<input id=\"" + id + "\" type=\"checkbox\" /><label for=\"" + id + "\" class=\"" + format + "_button\">" + label + "</label>").button());
           button = jQuery("#" + id, buttonset);
           button.attr("hallo-command", format);
+          button.addClass(format);
           button.bind("change", function(event) {
             format = jQuery(this).attr("hallo-command");
             return widget.options.editable.execute(format);
@@ -26,24 +32,26 @@
           queryState = function(event) {
             if (document.queryCommandState(format)) {
               button.attr("checked", true);
+              button.next("label").addClass("ui-state-clicked");
               return button.button("refresh");
             } else {
               button.attr("checked", false);
+              button.next("label").removeClass("ui-state-clicked");
               return button.button("refresh");
             }
           };
-          element = this.element;
-          this.element.bind("halloenabled", function() {
+          element = _this.element;
+          _this.element.bind("halloenabled", function() {
             return element.bind("keyup paste change mouseup", queryState);
           });
-          return this.element.bind("hallodisabled", function() {
+          return _this.element.bind("hallodisabled", function() {
             return element.unbind("keyup paste change mouseup", queryState);
           });
-        }, this);
+        };
         _ref = this.options.formattings;
-        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-          format = _ref[_i];
-          buttonize(format);
+        for (format in _ref) {
+          enabled = _ref[format];
+          if (enabled) buttonize(format);
         }
         buttonset.buttonset();
         return this.options.toolbar.append(buttonset);
@@ -51,4 +59,3 @@
       _init: function() {}
     });
   })(jQuery);
-}).call(this);
