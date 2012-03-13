b06f191cf7adc361e15f2a78bedef87c68a2e857
Henri Bergius <henri.bergius@iki.fi>
2012-03-13 19:49:23 +0100

    Provide clearer error on missing editor widgets

--- src/jquery.Midgard.midgardEditable.js
+++ src/jquery.Midgard.midgardEditable.js
@@ -28,6 +28,9 @@
             enableEditor: function(data) {
               	var widgetName = this.widgetName(data);
                 data.disabled = false;
+                if (typeof jQuery(data.element)[widgetName] !== 'function') {
+                  throw new Error(widgetName + ' widget is not available');
+                }
               	jQuery(data.element)[widgetName](data);
               	jQuery(data.element).data('createWidgetName', widgetName);
               	return jQuery(data.element);
