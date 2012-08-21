dc26f77e8e4b2d621f243ca4a77636925a3fd364
Henri Bergius <henri.bergius@iki.fi>
2012-08-21 15:04:27 +0200

    Reorganize built files so main license header is on top

--- examples/create.js
+++ examples/create.js
@@ -1,145 +1,3 @@
-//     Create.js - On-site web editing interface
-//     (c) 2011-2012 Henri Bergius, IKS Consortium
-//     Create may be freely distributed under the MIT license.
-//     For all details and documentation:
-//     http://createjs.org/
-(function (jQuery, undefined) {
-  // # Widget for adding items to a collection
-  jQuery.widget('Midgard.midgardCollectionAdd', {
-    addButton: null,
-
-    options: {
-      editingWidgets: null,
-      collection: null,
-      model: null,
-      view: null,
-      disabled: false,
-      vie: null,
-      editableOptions: null
-    },
-
-    _create: function () {
-      var widget = this;
-      if (!widget.options.collection.localStorage) {
-        widget.options.collection.url = widget.options.model.url();
-      }
-
-      widget.options.view.collection.bind('add', function (model) {
-        model.primaryCollection = widget.options.collection;
-        widget.options.vie.entities.add(model);
-        model.collection = widget.options.collection;
-      });
-
-      widget._bindCollectionView(widget.options.view);
-    },
-
-    _bindCollectionView: function (view) {
-      var widget = this;
-      view.bind('add', function (itemView) {
-        //itemView.el.effect('slide');
-        widget._makeEditable(itemView);
-      });
-    },
-
-    _makeEditable: function (itemView) {
-      this.options.editableOptions.disabled = this.options.disabled;
-      this.options.editableOptions.model = itemView.model;
-      jQuery(itemView.el).midgardEditable(this.options.editableOptions);
-    },
-
-    _init: function () {
-      if (this.options.disabled) {
-        this.disable();
-        return;
-      }
-      this.enable();
-    },
-
-    enable: function () {
-      var widget = this;
-      widget.addButton = jQuery('<button class="btn"><i class="icon-plus"></i> Add</button>').button();
-      widget.addButton.addClass('midgard-create-add');
-      widget.addButton.click(function () {
-        widget.options.collection.add({});
-      });
-
-      jQuery(widget.options.view.el).after(widget.addButton);
-    },
-
-    disable: function () {
-      if (this.addButton) {
-        this.addButton.remove();
-        delete this.addButton;
-      }
-    }
-  });
-})(jQuery);
-//     Create.js - On-site web editing interface
-//     (c) 2011-2012 Henri Bergius, IKS Consortium
-//     Create may be freely distributed under the MIT license.
-//     For all details and documentation:
-//     http://createjs.org/
-(function (jQuery, undefined) {
-  // # Widget for adding items anywhere inside a collection
-  jQuery.widget('Midgard.midgardCollectionAddBetween', jQuery.Midgard.midgardCollectionAdd, {
-    addButtons: [],
-
-    _bindCollectionView: function (view) {
-      var widget = this;
-      view.bind('add', function (itemView) {
-        //itemView.el.effect('slide');
-        widget._makeEditable(itemView);
-        widget._refreshButtons();
-      });
-      view.bind('remove', function () {
-        widget._refreshButtons();
-      });
-    },
-
-    _refreshButtons: function () {
-      var widget = this;
-      window.setTimeout(function () {
-        widget.disable();
-        widget.enable();
-      }, 1);
-    },
-
-    prepareButton: function (index) {
-      var widget = this;
-      var addButton = jQuery('<button class="btn"><i class="icon-plus"></i></button>').button();
-      addButton.addClass('midgard-create-add');
-      addButton.click(function () {
-        widget.options.collection.add({}, {
-          at: index
-        });
-      });
-      return addButton;
-    },
-
-    enable: function () {
-      var widget = this;
-
-      var firstAddButton = widget.prepareButton(0);
-      jQuery(widget.options.view.el).before(firstAddButton);
-      widget.addButtons.push(firstAddButton);
-
-      jQuery.each(widget.options.view.entityViews, function (cid, view) {
-        var index = widget.options.collection.indexOf(view.model);
-        var addButton = widget.prepareButton(index + 1);
-        jQuery(view.el).after(addButton);
-        widget.addButtons.push(addButton);
-      });
-    },
-
-    disable: function () {
-      var widget = this;
-      jQuery.each(widget.addButtons, function (idx, button) {
-        button.remove();
-      });
-      widget.addButtons = [];
-    }
-  });
-})(jQuery);
 //     Create.js 1.0.0alpha2 - On-site web editing interface
 //     (c) 2011-2012 Henri Bergius, IKS Consortium
 //     Create may be freely distributed under the MIT license.
@@ -439,6 +297,148 @@
       this._trigger('statechange', null, {
         state: 'browse'
       });
+    }
+  });
+})(jQuery);
+//     Create.js - On-site web editing interface
+//     (c) 2011-2012 Henri Bergius, IKS Consortium
+//     Create may be freely distributed under the MIT license.
+//     For all details and documentation:
+//     http://createjs.org/
+(function (jQuery, undefined) {
+  // # Widget for adding items to a collection
+  jQuery.widget('Midgard.midgardCollectionAdd', {
+    addButton: null,
+
+    options: {
+      editingWidgets: null,
+      collection: null,
+      model: null,
+      view: null,
+      disabled: false,
+      vie: null,
+      editableOptions: null
+    },
+
+    _create: function () {
+      var widget = this;
+      if (!widget.options.collection.localStorage) {
+        widget.options.collection.url = widget.options.model.url();
+      }
+
+      widget.options.view.collection.bind('add', function (model) {
+        model.primaryCollection = widget.options.collection;
+        widget.options.vie.entities.add(model);
+        model.collection = widget.options.collection;
+      });
+
+      widget._bindCollectionView(widget.options.view);
+    },
+
+    _bindCollectionView: function (view) {
+      var widget = this;
+      view.bind('add', function (itemView) {
+        //itemView.el.effect('slide');
+        widget._makeEditable(itemView);
+      });
+    },
+
+    _makeEditable: function (itemView) {
+      this.options.editableOptions.disabled = this.options.disabled;
+      this.options.editableOptions.model = itemView.model;
+      jQuery(itemView.el).midgardEditable(this.options.editableOptions);
+    },
+
+    _init: function () {
+      if (this.options.disabled) {
+        this.disable();
+        return;
+      }
+      this.enable();
+    },
+
+    enable: function () {
+      var widget = this;
+      widget.addButton = jQuery('<button class="btn"><i class="icon-plus"></i> Add</button>').button();
+      widget.addButton.addClass('midgard-create-add');
+      widget.addButton.click(function () {
+        widget.options.collection.add({});
+      });
+
+      jQuery(widget.options.view.el).after(widget.addButton);
+    },
+
+    disable: function () {
+      if (this.addButton) {
+        this.addButton.remove();
+        delete this.addButton;
+      }
+    }
+  });
+})(jQuery);
+//     Create.js - On-site web editing interface
+//     (c) 2011-2012 Henri Bergius, IKS Consortium
+//     Create may be freely distributed under the MIT license.
+//     For all details and documentation:
+//     http://createjs.org/
+(function (jQuery, undefined) {
+  // # Widget for adding items anywhere inside a collection
+  jQuery.widget('Midgard.midgardCollectionAddBetween', jQuery.Midgard.midgardCollectionAdd, {
+    addButtons: [],
+
+    _bindCollectionView: function (view) {
+      var widget = this;
+      view.bind('add', function (itemView) {
+        //itemView.el.effect('slide');
+        widget._makeEditable(itemView);
+        widget._refreshButtons();
+      });
+      view.bind('remove', function () {
+        widget._refreshButtons();
+      });
+    },
+
+    _refreshButtons: function () {
+      var widget = this;
+      window.setTimeout(function () {
+        widget.disable();
+        widget.enable();
+      }, 1);
+    },
+
+    prepareButton: function (index) {
+      var widget = this;
+      var addButton = jQuery('<button class="btn"><i class="icon-plus"></i></button>').button();
+      addButton.addClass('midgard-create-add');
+      addButton.click(function () {
+        widget.options.collection.add({}, {
+          at: index
+        });
+      });
+      return addButton;
+    },
+
+    enable: function () {
+      var widget = this;
+
+      var firstAddButton = widget.prepareButton(0);
+      jQuery(widget.options.view.el).before(firstAddButton);
+      widget.addButtons.push(firstAddButton);
+
+      jQuery.each(widget.options.view.entityViews, function (cid, view) {
+        var index = widget.options.collection.indexOf(view.model);
+        var addButton = widget.prepareButton(index + 1);
+        jQuery(view.el).after(addButton);
+        widget.addButtons.push(addButton);
+      });
+    },
+
+    disable: function () {
+      var widget = this;
+      jQuery.each(widget.addButtons, function (idx, button) {
+        button.remove();
+      });
+      widget.addButtons = [];
     }
   });
 })(jQuery);
