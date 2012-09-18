86a436f5759b7bed0a8e07eec8d853fba822847c
Henri Bergius <henri.bergius@iki.fi>
2012-09-18 21:48:15 +0200

    Build with German

--- examples/create.js
+++ examples/create.js
@@ -3088,6 +3088,35 @@ if (window.midgardCreate.locale === undefined) {
   window.midgardCreate.locale = {};
 }
 
+window.midgardCreate.locale.de = {
+  // Session-state buttons for the main toolbar
+  'Save': 'Speichern',
+  'Saving': 'Speichert',
+  'Cancel': 'Abbrechen',
+  'Edit': 'Bearbeiten',
+  // Storage status messages
+  'localModifications': '<%= number %> Einträge auf dieser Seite haben lokale Modifikationen',
+  'Restore': 'Wiederherstellen',
+  'Ignore': 'Ignorieren',
+  'saveSuccess': 'Eintrag "<%= label %>" erfolgreich gespeichert',
+  'saveSuccessMultiple': '<%= number %> Einträge erfolgreich gespeichert',
+  'saveError': 'Beim Speichern trat ein Fehler auf<br /><%= error %>',
+  // Tagging
+  'Item tags': 'Schlagwörter des Eintrags',
+  'Suggested tags': 'Schlagwortvorschläge',
+  'Tags': 'Schlagwörter',
+  'add a tag': 'Schlagwort hinzufügen',
+  // Collection widgets
+  'Add': 'Hinzufügen',
+  'Choose type to add': 'Typ zum Hinzufügen wählen'
+};
+if (window.midgardCreate === undefined) {
+  window.midgardCreate = {};
+}
+if (window.midgardCreate.locale === undefined) {
+  window.midgardCreate.locale = {};
+}
+
 window.midgardCreate.locale.en = {
   // Session-state buttons for the main toolbar
   'Save': 'Save',
