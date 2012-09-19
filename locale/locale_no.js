de60b179b09d8c5ec87ea0d209710172f3a99f29
Harald Kirkerød <haraball@gmail.com>
2012-09-19 09:03:44 +0200

    Adding norwegian translation

---
+++ locale/locale_no.js
@@ -0,0 +1,29 @@
+if (window.midgardCreate === undefined) {
+  window.midgardCreate = {};
+}
+if (window.midgardCreate.locale === undefined) {
+  window.midgardCreate.locale = {};
+}
+
+window.midgardCreate.locale.en = {
+  // Session-state buttons for the main toolbar
+  'Save': 'Lagre',
+  'Saving': 'Lagrer',
+  'Cancel': 'Avbryt',
+  'Edit': 'Rediger',
+  // Storage status messages
+  'localModifications': '<%= number %> elementer på denne siden er modifisert lokalt',
+  'Restore': 'Gjenopprett',
+  'Ignore': 'Ignorer',
+  'saveSuccess': 'Element "<%= label %>" ble lagret',
+  'saveSuccessMultiple': '<%= number %> elementer ble lagret',
+  'saveError': 'En feil oppstod under lagring<br /><%= error %>',
+  // Tagging
+  'Item tags': 'Element-tagger',
+  'Suggested tags': 'Anbefalte tagger',
+  'Tags': 'Tagger',
+  'add a tag': 'legg til tagg',
+  // Collection widgets
+  'Add': 'Legg til',
+  'Choose type to add': 'Velg type å legge til'
+};
