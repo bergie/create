22a437939379d938d03d1fdbd4bdddb23c2c28d7
Henri Bergius <henri.bergius@iki.fi>
2012-09-26 09:54:59 +0200

    Build with Swedish

--- examples/create.js
+++ examples/create.js
@@ -3527,6 +3527,36 @@ window.midgardCreate.locale.ru = {
 if (window.midgardCreate === undefined) {
   window.midgardCreate = {};
 }
+if (window.midgardCreate.locale === undefined) {
+  window.midgardCreate.locale = {};
+}
+
+window.midgardCreate.locale.sv = {
+  // Session-state buttons for the main toolbar
+  'Save': 'Spara',
+  'Saving': 'Sparar',
+  'Cancel': 'Avbryt',
+  'Edit': 'Redigera',
+  // Storage status messages
+  'localModification': 'Elementet "<%= label %>" har lokala förändringar',
+  'localModifications': '<%= number %> element på den här sidan har lokala förändringar',
+  'Restore': 'Återställ',
+  'Ignore': 'Ignorera',
+  'saveSuccess': 'Elementet "<%= label %>" sparades',
+  'saveSuccessMultiple': '<%= number %> element sparades',
+  'saveError': 'Ett fel uppstod under sparande<br /><%= error %>',
+  // Tagging
+  'Item tags': 'Element-taggar',
+  'Suggested tags': 'Föreslagna taggar',
+  'Tags': 'Taggar',
+  'add a tag': 'lägg till en tagg',
+  // Collection widgets
+  'Add': 'Lägg till',
+  'Choose type to add': 'Välj typ att lägga till'
+};
+if (window.midgardCreate === undefined) {
+  window.midgardCreate = {};
+}
 
 window.midgardCreate.localize = function (id, language) {
   if (!window.midgardCreate.locale) {
