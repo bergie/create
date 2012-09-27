1a873c3b1c47c2bed8525354bd8e14ab87414885
Henri Bergius <henri.bergius@iki.fi>
2012-09-27 15:29:15 +0200

    Ensure contentEditables don't stay focused after disabling them

--- src/jquery.Midgard.midgardEditable.js
+++ src/jquery.Midgard.midgardEditable.js
@@ -257,6 +257,10 @@
         // only if there has been an editing widget registered
         jQuery(data.element)[widgetName](data);
         jQuery(data.element).removeClass('ui-state-disabled');
+
+        if (data.element.is(':focus')) {
+          data.element.blur();
+        }
       }
     },
 
