67d3a9e9486cc7a6f74cb5b32bedcecff9a88c32
Henri Bergius <henri.bergius@iki.fi>
2012-11-04 16:45:56 +0100

    Initial integration with CKEditor 4 beta

--- examples/create.js
+++ examples/create.js
@@ -829,7 +829,8 @@
       if (_.isFunction(this.options.decorate)) {
         this.options.decorate({
           editable: this,
-          element: this.element
+          element: this.element,
+          entity: this.options.model
         });
       }
     },
@@ -1214,6 +1215,7 @@
           editable: this.options.widget,
           editor: this,
           predicate: this.options.property,
+          entity: this.options.entity,
           element: this.element
         });
       }
@@ -1324,6 +1326,57 @@
     disable: function () {
       Aloha.jQuery(this.options.element.get(0)).mahalo();
       this.options.disabled = true;
+    }
+  });
+})(jQuery);
+//     Create.js - On-site web editing interface
+//     (c) 2012 Tobias Herrmann, IKS Consortium
+//     Create may be freely distributed under the MIT license.
+//     For all details and documentation:
+(function (jQuery, undefined) {
+  // Run JavaScript in strict mode
+  /*global jQuery:false _:false document:false */
+  'use strict';
+
+  // # CKEditor editing widget
+  //
+  // This widget allows editing textual content areas with the
+  // [CKEditor](http://ckeditor.com/) rich text editor.
+  jQuery.widget('Create.ckeditorWidget', jQuery.Create.editWidget, {
+    enable: function () {
+      this.element.attr('contentEditable', 'true');
+      this.editor = CKEDITOR.inline(this.element.get(0));
+      this.options.disabled = false;
+
+      var widget = this;
+      this.editor.on('focus', function () {
+        widget.options.activated();
+      });
+      this.editor.on('blur', function () {
+        widget.options.activated();
+      });
+      this.editor.on('key', function () {
+        widget.options.changed(widget.editor.getData());
+      });
+      this.editor.on('paste', function () {
+        widget.options.changed(widget.editor.getData());
+      });
+      this.editor.on('afterCommandExec', function () {
+        widget.options.changed(widget.editor.getData());
+      });
+    },
+
+    disable: function () {
+      if (!this.editor) {
+        return;
+      }
+      this.element.attr('contentEditable', 'false');
+      this.editor.destroy();
+      this.editor = null;
+    },
+
+    _initialize: function () {
+      CKEDITOR.disableAutoInline = true;
     }
   });
 })(jQuery);
