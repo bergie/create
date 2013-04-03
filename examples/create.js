0be970c16dcfd7d8a8bf0ff58d430e8596ba3291
Henri Bergius <henri.bergius@iki.fi>
2013-04-03 15:46:30 +0200

    Rebuild, refs #167

--- examples/create.js
+++ examples/create.js
@@ -1437,6 +1437,11 @@
       this.editor.on('afterCommandExec', function () {
         widget.options.changed(widget.editor.getData());
       });
+      this.editor.on('configLoaded', function() {
+        jQuery.each(widget.options.editorOptions, function(optionName, option) {
+          widget.editor.config[optionName] = option;
+        });
+      });
     },
 
     disable: function () {
