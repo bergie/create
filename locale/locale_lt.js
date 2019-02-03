9f202355e5b55dd2b14a0faf13e26368e3a943da
Luke Rotherfield <l.rotherfield@gmail.com>
2019-02-02 21:15:06 -0500

    Hot Fix: lt locale file overwriting en locale

--- locale/locale_lt.js
+++ locale/locale_lt.js
@@ -5,7 +5,7 @@ if (window.midgardCreate.locale === undefined) {
     window.midgardCreate.locale = {};
 }
 
-window.midgardCreate.locale.en = {
+window.midgardCreate.locale.lt = {
     // Session-state buttons for the main toolbar
     'Save': 'Saugoti',
     'Saving': 'Saugojama',
